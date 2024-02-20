e15af38420ede3d245f475b413063d45e5245c0c
PhiPGN <phipgn@gmail.com>
2024-02-20 22:41:19 +0700

    update

--- src/test/java/com/giaphi/demo/tests/LoginTest.java
+++ src/test/java/com/giaphi/demo/tests/LoginTest.java
@@ -5,10 +5,8 @@ import com.giaphi.demo.pages.LoginPage;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
