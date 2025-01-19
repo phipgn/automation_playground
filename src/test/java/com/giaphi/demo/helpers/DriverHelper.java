73e9c6694ce35575bebdcb78487dea1f00cd890d
PhiPGN <phipgn@gmail.com>
2025-01-19 15:53:03 +0700

    Fix logs

--- src/test/java/com/giaphi/demo/helpers/DriverHelper.java
+++ src/test/java/com/giaphi/demo/helpers/DriverHelper.java
@@ -1,16 +1,10 @@
 package com.giaphi.demo.helpers;
 
 import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverHelper {
@@ -33,29 +27,17 @@ public class DriverHelper {
 
     public static WebDriver getDriver() {
         WebDriverManager.chromedriver().setup();
-        var options = new ChromeOptions();
+        
         var path = System.getProperty("user.dir");
+        var options = new ChromeOptions();
         var logFile = new File(path + "/chromedriver.log");
-        options.setCapability("goog:loggingPrefs", logFile);
-        return new ChromeDriver(options);
+        var service = new ChromeDriverService.Builder()
+            // .usingDriverExecutable(new File(path + "/drivers/chromedriver.exe"))
+            .withVerbose(true)
+            .withLogFile(logFile)
+            .build();
 
-        // To manually manage driver binaries
-        // var path = System.getProperty("user.dir");
-        // var options = new ChromeOptions();
-        // var logFile = new File(path + "/chromedriver.log");
-        // var service = new ChromeDriverService.Builder()
-        //     .usingDriverExecutable(new File(path + "/drivers/chromedriver.exe"))
-        //     .withVerbose(true)
-        //     .withLogFile(logFile)
-        //     .build();
-        // return new ChromeDriver(service, options);
-
-        // These lines for Linux & MacOS
-    //  System.setProperty("webdriver.chrome.driver", path + "/drivers/chromedriver");
-//         options.addArguments("--no-sandbox");
-//         options.addArguments("--headless");
-//         options.addArguments("--disable-dev-shm-usage");
-//         options.addArguments("--window-size=1920x1080");
+        return new ChromeDriver(service, options);
     }
 }
 
--- src/test/java/com/giaphi/demo/tests/BaseTest.java
+++ src/test/java/com/giaphi/demo/tests/BaseTest.java
@@ -1,9 +1,8 @@
 package com.giaphi.demo.tests;
 
+import com.giaphi.demo.helpers.DriverHelper;
 import com.giaphi.demo.pages.LoginPage;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class BaseTest {
@@ -14,8 +13,7 @@ public class BaseTest {
     public BaseTest() { }
 
     protected WebDriver setUpDriver() {
-        WebDriverManager.chromedriver().setup();
-        var driver = new ChromeDriver();
+        var driver = DriverHelper.getDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get(BASE_URL);
