69d18aef5c90f42cbe2db1eba7b1d49915f51f09
PhiPGN <phipgn@gmail.com>
2025-02-15 16:06:19 +0700

    Sua bai tap

--- java-playground/src/test/java/com/giaphi/Main.java
+++ java-playground/src/test/java/com/giaphi/Main.java
@@ -1,34 +1,24 @@
 package com.giaphi;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // var n = 123456;
-        // var t = n;
-        // var t2 = n;
-    
-        // var u = 1;
-        // do {
-        //     t2 = t2 / 10;
-        //     u = u * 10;
-        // } while(t2 > 1);
-        
-        // var r = 0;
-        // do {
-        //     r = r + t % 10 * u;
-        //     u = u / 10;
-        //     t = t / 10;
-        // } while (t > 0);
-        
-        // System.out.print(r);
-        var n = 123456;
-        var r = 0;
-        while (n > 0) {
-            r = r * 10 + n % 10;
-            n = n / 10;
+        var numbers = new int[10];
+        var random = new Random();
+        for (var i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(100);
+            System.out.print(numbers[i] + " ");
         }
-        System.out.print(r);
+
+        var max = numbers[0];
+        for (var i = 0; i < numbers.length; i++) {
+            if (max < numbers[i]) {
+                max = numbers[i];
+            }
+        }
+        System.out.println("Max number: " + max);
     }
 }
